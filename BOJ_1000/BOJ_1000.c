53743a094af05baa6fba40b65cb7a7f7f1e14f11
asharien <136838460+asharien@users.noreply.github.com>
2023-11-12 01:32:30 +0900

    Create BOJ_1000.c

---
+++ BOJ_1000/BOJ_1000.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+int main(){
+
+        int A, B;
+        scanf("%d %d", &A, &B);
+        int C = A+B;
+        printf("%d", C);
+        printf("\n");
+        return 0;
+}
