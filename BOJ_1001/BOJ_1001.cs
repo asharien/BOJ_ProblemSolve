2006d9061783c8bb249989027d00594ece65f1b8
asharien <136838460+asharien@users.noreply.github.com>
2023-11-25 01:01:53 +0900

    Create BOJ_1001.cs

---
+++ BOJ_1001/BOJ_1001.cs
@@ -0,0 +1,11 @@
+using System;
+public class math{
+        static void Main(){
+                var line = Console.ReadLine();
+                var data =line.Split(' ');
+                int A = int.Parse(data[0]);
+                int B = int.Parse(data[1]);
+                int C = A-B;
+                Console.WriteLine(C);
+        }
+}
