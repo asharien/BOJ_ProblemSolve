c261fe864b106cdfcd39e58fa93c100920f0dab1
asharien <136838460+asharien@users.noreply.github.com>
2023-12-09 22:43:31 +0900

    Create BOJ_1076.py

---
+++ BOJ_1076/BOJ_1076.py
@@ -0,0 +1,3 @@
+import sys
+RESIST = {"black":0, "brown":1, "red":2, "orange":3, "yellow":4, "green":5, "blue":6, "violet":7, "grey":8, "white":9}
+print(((RESIST[str(sys.stdin.readline().rstrip())])*10+(RESIST[str(sys.stdin.readline().rstrip())]))*10**RESIST[str(sys.stdin.readline().rstrip())])
