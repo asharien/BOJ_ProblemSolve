153378336863ff57fb93ec49a89e963e2be4fd4a
asharien <136838460+asharien@users.noreply.github.com>
2024-01-13 01:48:42 +0900

    Create BOJ_1402.java

---
+++ BOJ_1402/BOJ_1402.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+class Main {
+	public static void main(String[] args) 
+	throws IOException{
+		BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder SB = new StringBuilder();
+		int testCases = Integer.parseInt(BR.readLine());
+        for(int i=0; i<testCases; i++){
+            StringTokenizer ST = new StringTokenizer(BR.readLine());
+            BigInteger A = new BigInteger(ST.nextToken());
+            BigInteger B = new BigInteger(ST.nextToken());
+            if(A.compareTo(B)>=-1){
+                SB.append("yes\n");
+            }
+            else{
+                SB.append("no\n");
+            }
+        }
+        BW.write(SB+"");
+        BW.flush();
+    }
+}
