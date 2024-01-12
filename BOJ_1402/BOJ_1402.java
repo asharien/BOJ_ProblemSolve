7b47dfee1b763a637410b1bb893407f29a94c5cf
asharien <136838460+asharien@users.noreply.github.com>
2024-01-13 01:50:10 +0900

    Update BOJ_1402.java

--- BOJ_1402/BOJ_1402.java
+++ BOJ_1402/BOJ_1402.java
@@ -8,18 +8,18 @@ class Main {
 		BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder SB = new StringBuilder();
 		int testCases = Integer.parseInt(BR.readLine());
-        for(int i=0; i<testCases; i++){
-            StringTokenizer ST = new StringTokenizer(BR.readLine());
-            BigInteger A = new BigInteger(ST.nextToken());
-            BigInteger B = new BigInteger(ST.nextToken());
-            if(A.compareTo(B)>=-1){
-                SB.append("yes\n");
-            }
-            else{
-                SB.append("no\n");
-            }
-        }
-        BW.write(SB+"");
-        BW.flush();
-    }
+        	for(int i=0; i<testCases; i++){
+			StringTokenizer ST = new StringTokenizer(BR.readLine());
+			BigInteger A = new BigInteger(ST.nextToken());
+			BigInteger B = new BigInteger(ST.nextToken());
+			if(A.compareTo(B)>=-1){
+				SB.append("yes\n");
+			}
+			else{
+				SB.append("no\n");
+			}
+		}
+		BW.write(SB+"");
+		BW.flush();
+	}
 }
