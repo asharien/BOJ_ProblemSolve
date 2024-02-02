5a2d7508d587188d84d6b268e8861753d400195f
asharien <asharien@naver.com>
2024-02-02 17:29:34 +0900

    Upload from Termux Fri Feb  2 17:29:33 KST 2024

---
+++ BOJ_1253/BOJ_1253.py
@@ -0,0 +1,21 @@
+import sys
+N = int(sys.stdin.readline())
+L = list(sorted(map(int, sys.stdin.readline().split())))
+A = 0
+for i in range(len(L)):
+    TARGET = L[i]
+    TMP = L.copy()
+    del TMP[i]
+    UP, DW = 0, len(TMP)-1
+    while UP<DW:
+        S = TMP[UP] + TMP[DW]
+        if S<TARGET: UP += 1
+        elif S>TARGET: DW -= 1
+        else:
+            A += 1
+            UP += 1
+            DW -= 1
+            break
+print(A)
+
+
