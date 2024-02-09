afa8fa30b600e15439f72a0ed80fea86e6cc7428
asharien <136838460+asharien@users.noreply.github.com>
2024-02-09 23:29:52 +0900

    Create BOJ_2455.py

---
+++ BOJ_2455/BOJ_2455.py
@@ -0,0 +1,8 @@
+import sys
+ANS, PLACE_HOLDER = 0, 0
+for _ in range(4):
+    N, M = map(int, sys.stdin.readline().split())
+    ANS -= N
+    ANS += M
+    if(ANS>PLACE_HOLDER):PLACE_HOLDER = ANS
+print(PLACE_HOLDER)
